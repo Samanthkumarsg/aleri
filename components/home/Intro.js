e27cd86527d3b637d34519a44d9a9a41fe4f2704
samanth kumar <samanthkumarsg@gmail.com>
2022-01-28 09:32:50 +0530

    Initial commit

---
+++ components/home/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <div className='flex items-center justify-center flex-col border-double border-4 py-24 bg-white rounded-md '>
+            <p className='text-[2.2rem] font-extrabold text-gray-700'>ಅಲೇರಿ ಶ್ರೀ ಸತ್ಯ ಸಾರಮಾನಿ ಕಾನದ ಕಟದ ಮೂಲ ಕ್ಷೇತ್ರ (ರಿ)  </p>
+            <p className='text-xl text-gray-500 w-3/4 font-semibold text-center mt-4 antialiased leading-relaxed '>
+                ಕ್ಷೇತ್ರದ ಅಭಿವೃಧ್ಧಿ ಕಾರ್ಯಕ್ರಮ ಹಾಗೂ ಕ್ಷೇತ್ರಕ್ಕೆ ಸಂಬಂಧಿಸಿದ ಎಲ್ಲಾ ಮಾಹಿತಿಗಳನ್ನು ಅಂತರ್ಜಾಲದ ಮೂಲಕ ಸುಲಭವಾಗಿ ಜನ ಸಮೂಹವನ್ನು ತಲುಪಿಸುವ ಉದ್ದೇಶಕ್ಕಾಗಿ ಈ ವೆಬ್ ಸೈಟನ್ನು ನಿರ್ಮಿಸಲಾಗಿದೆ
+
+            </p>
+        </div >
+    )
+}
+
+export default Intro;
---
+++ containers/HomeContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Intro from '../components/home/Intro';
+
+function HomeContainer() {
+    return (
+        <React.Fragment>
+            <div className='container mx-auto py-8 h-screen bg-gray-50 '>
+                <Intro />
+
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default HomeContainer;
