1bd7bb7674454259cdd2aa6d1aab0d2d25760eba
samanth kumar <samanthkumarsg@gmail.com>
2022-01-28 16:54:20 +0530

    Events redirected to whatsapp

---
+++ components/events/EventModel.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Router from 'next/router'
+
+function EventModel({ toggleHostEvent }) {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        var data = new FormData(event.target)
+        let formObject = Object.fromEntries(data.entries())
+        console.log(formObject);
+        window.open(`https://api.whatsapp.com/send/?phone=+919535485134&text=*Name :* ${formObject.name}%0a*Mobile :* ${formObject.mobile}%0a*Title :* ${formObject.title}%0a*Location :* ${formObject.location}%0a*Organisation Name :* ${formObject.organisationName}&app_absent=0`);
+        toggleHostEvent();
+    }
+    return (
+        <div className='mx-auto container h-full fixed top-0 bg-[transparent] backdrop-blur-sm flex items-center justify-center shadow-xl'>
+            <div className='border mx-auto bg-white w-96 p-5 shadow-xl'>
+                <div className='flex flex-col mb-3 border border-x-0 border-b-1 border-t-0  pb-3'>
+                    <h1 className='mb-1 font-bold text-blue-500'>Host Event</h1>
+                    <p className='text-gray-400 text-sm'>Please fill the details to host event</p>
+                </div>
+                <form className='flex flex-col ' onSubmit={(e) => handleSubmit(e)}>
+                    <div className='flex flex-col mb-4 '>
+                        <label className='text-sm text-gray-500 mb-1'>Name</label>
+                        <input type="text" className='border w-100 p-1 rounded' name="name" required />
+                    </div>
+                    <div className='flex flex-col mb-4'>
+                        <label className='text-sm text-gray-500 mb-1'>Mobile</label>
+                        <input type="text" className='border w-100 p-1 rounded' name="mobile" required />
+                    </div>
+                    <div className='flex flex-col mb-4'>
+                        <label className='text-sm text-gray-500 mb-1'>Title</label>
+                        <input type="text" className='border w-100 p-1 rounded' name="title" required />
+                    </div>
+                    <div className='flex flex-col mb-4'>
+                        <label className='text-sm text-gray-500 mb-1'>Location</label>
+                        <input type="text" className='border w-100 p-1 rounded' name="location" required />
+                    </div>
+                    <div className='flex flex-col mb-4'>
+                        <label className='text-sm text-gray-500 mb-1'>Organisation name</label>
+                        <input type="text" className='border w-100 p-1 rounded' name="organisationName" required />
+                    </div>
+                    <div className='flex flex-row mb-4'>
+                        <button className='border p-2 text-sm text-white w-1/2 rounded bg-blue-600' type='submit'>Submit</button>
+                        <button className='border p-2 text-sm text-white w-1/2 rounded bg-rose-600' onClick={toggleHostEvent}>Cancel</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default EventModel;
--- containers/EventContainer.js
+++ containers/EventContainer.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import DataTable from 'react-data-table-component';
+import EventModel from '../components/events/EventModel';
 
 function EventContainer() {
     const columns = [
@@ -61,7 +62,7 @@ function EventContainer() {
             title: 'Meeting regarding fund collection',
             date: '23/02/2022',
             location: 'Sulia',
-            host: 'ಅಲೇರಿ ಶ್ರೀ ಸತ್ಯ ಸಾರಮಾನಿ ಕಾನದ ಕಟದ ಮೂಲ ಕ್ಷೇತ್ರ (ರಿ)',
+            host: 'ಅಲೇರಿ ಶ್ರೀ ಸತ್ಯ ಸಾರಮಾನಿ ಕಾನದ ಕಟದ ಮೂಲ ಕ್ಷೇತ್ರ (ರಿ) ',
         },
 
     ];
@@ -77,15 +78,17 @@ function EventContainer() {
         headCells: {
             style: {
                 fontSize: '1em',
-                borderLeft: '1px solid #cbd5e1'
+                borderLeft: '1px solid #cbd5e1',
+                background: '#f1f5f9'
 
             },
         },
 
         cells: {
             style: {
-                fontSize: '1em',
-                borderLeft: '1px solid #cbd5e1'
+                fontSize: '1.1em',
+                borderLeft: '1px solid #cbd5e1',
+                fontWeight: '600'
             },
         },
         headRow: {
@@ -102,19 +105,25 @@ function EventContainer() {
         },
     };
 
+    const [show, setShow] = React.useState(false)
+    const toggleHostEvent = () => {
+        setShow(!show);
+    }
+
     return (
         <div className='container mx-auto py-0 h-screen bg-gray-50'>
-            <div className='h-20 border w-full bg-white flex items-center justify-between px-4 mt-4 rounded-sm'>
-                <div className='text-2xl font-semibold text-gray-600'>Events Near you</div>
-                <div className=''>
+            <div className=' border w-full bg-white flex flex-col items-start justify-between p-4 mt-4 rounded-sm md:flex-row md:items-center'>
+                <div className='text-2xl font-normal text-gray-600'>Events Near you</div>
+                <div className='flex space-x-4 py-3'>
                     <Link href='https://api.whatsapp.com/send/?phone=+919535485134&text=i would like to recieve notifications from aleri &app_absent=0'>
-                        <button className='border-2 px-3 py-1 rounded text-gray-500'> Subscribe</button>
+                        <button className='border-2 px-3 py-1 rounded text-gray-500 '> Subscribe</button>
                     </Link>
-                    <button className='border-2 px-3 py-1 rounded text-gray-500'> + Host Event</button>
+                    <button className='border-2 px-3 py-1 rounded text-gray-500' onClick={toggleHostEvent}> + Host Event</button>
+
 
                 </div>
             </div>
-            <div className='h-1/2'>
+            <div className='h-1/2 mt-4'>
                 <DataTable
                     columns={columns}
                     data={data}
@@ -122,6 +131,8 @@ function EventContainer() {
 
                 />
             </div>
+            {/* EVENT MODAL */}
+            {show ? <EventModel toggleHostEvent={toggleHostEvent} /> : ""}
         </div>
     );
 }
